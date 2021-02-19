7012b98e6b2e432adec37e0558a6f9d4e7867431
Deepan-kishore <dpnkishore@gmail.com>
2021-02-19 21:20:59 +0530

    Spring project started from spring intialiser and basic controller formed and tested

---
+++ src/main/java/com/crud/example/demo/controller/controller.java
@@ -0,0 +1,32 @@
+package com.crud.example.demo.controller;
+
+import com.crud.example.demo.Model.Model;
+import com.crud.example.demo.service.Ques_service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @Autowired
+    private  Ques_service ques;
+
+    @GetMapping("/")
+    public String healthCheck(){
+        return "OK";
+    }
+
+@GetMapping("/mappingTest")
+    public  Model Test(){
+
+//         Model question = new Model(1,"Sample_question","Quest_Desc","Answer");
+//    ques.Single_creation();
+//    ques.Single_creation();
+return ques.Single_creation();
+//    return question;
+    }
+
+}
